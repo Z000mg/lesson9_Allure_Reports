33dfb7d95df7b2d3ca755709d4a3d4aea43c0dd4
z000mg <zooomg2015@yandex.ru>
2020-12-15 13:28:27 +0300

    steps, faker, attachments, allure, remote

--- src/test/java/tests/FillFormTests.java
+++ src/test/java/tests/FillFormTests.java
@@ -23,7 +23,7 @@ import static helpers.AttachmentsHelper.*;
 import static io.qameta.allure.Allure.step;
 
 public class FillFormTests {
-    File file = new File("src/test/resources/1.jpg");
+    File file = new File(".idea/.gitignore");
 
     @BeforeAll
     static void setup(){
@@ -94,7 +94,7 @@ public class FillFormTests {
             $("tbody").$(byText("Mobile")).parent().shouldHave(text(userNumber));
             $("tbody").$(byText("Date of Birth")).parent().shouldHave(text("07 July,1973"));
             $("tbody").$(byText("Hobbies")).parent().shouldHave(text("Sports, Music"));
-            $("tbody").$(byText("Picture")).parent().shouldHave(text("1.jpg"));
+            $("tbody").$(byText("Picture")).parent().shouldHave(text("gitignore"));
             $("tbody").$(byText("Address")).parent().shouldHave(text(currentAddress));
             $("tbody").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
         });
@@ -107,6 +107,6 @@ public class FillFormTests {
         attachPageSource();
         attachAsText("Browser console logs", getConsoleLogs());
         attachVideo();
-        //closeWebDriver();
+        closeWebDriver();
     }
 }
